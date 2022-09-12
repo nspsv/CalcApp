57741421e12cef43c69067b95deb637bafe3c519
JNTUK <JNTUK@DESKTOP-B3LMS05>
2022-09-12 17:34:19 +0530

    doDiv to dodiv

--- src/main/java/ETA_SB/jntu_calc_application/Calculator.java
+++ src/main/java/ETA_SB/jntu_calc_application/Calculator.java
@@ -14,8 +14,9 @@ public class Calculator {
 		return num1 * num2;
 	}
 
-	public double doDiv(double num1, double num2) {
+	public double dodiv(double num1, double num2) {
 		return num1 / num2;
 	}
+	
 
 }
