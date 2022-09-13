98d1d0a06342a5a430c537881f43373a86396a2a
JNTUK <JNTUK@DESKTOP-B3LMS05>
2022-09-13 14:46:53 +0530

    Added doRem method

--- src/main/java/ETA_SB/jntu_calc_application/Calculator.java
+++ src/main/java/ETA_SB/jntu_calc_application/Calculator.java
@@ -17,6 +17,8 @@ public class Calculator {
 	public double doDiv(double num1, double num2) {
 		return num1 / num2;
 	}
-	
+	public double doRem (double num1,double num2 ) {
+		return num1 % num2;
+	}
 
 }
